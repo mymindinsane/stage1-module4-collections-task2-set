354d8143dc5370667d08ce76a6b7ddf813e0f842
mymindinsane <arturguletskiy21@gmail.com>
2024-07-28 18:23:37 +0300

    done SubsetOfSquaresCreator.java

--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -2,9 +2,14 @@ package com.epam.mjc.collections.set;
 
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SubsetOfSquaresCreator {
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
-        return Set.of();
+        TreeSet<Integer> powerOfTwoSet = new TreeSet<>();
+        for (Integer i : sourceList) {
+            powerOfTwoSet.add((int) Math.pow(i, 2));
+        }
+        return powerOfTwoSet.subSet(lowerBound, upperBound + 1);
     }
 }
